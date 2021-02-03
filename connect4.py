63794e1e7a062bb35a487f956098c2de85f32ce3
MiriPV <mirandaverheij93@gmail.com>
2021-02-03 13:37:36 +0100

    Asking for input of players

--- connect4.py
+++ connect4.py
@@ -9,8 +9,14 @@ game_over = False
 turn = 0
 
 while not game_over:
-    #ask for player 1 input
+    #Ask for input player 1
     if turn == 0:
-        selection = input("Player 1 Make your selection (0-6)")
+        selection = int(input("Player 1, Make your selection(0-6): "))
 
-    #ask for player 2 input
+
+    #Ask for input player 2
+    else: 
+        selection = int(input("Player 2, Make your selection(0-6): "))
+
+    turn += 1
+    turn = turn % 2
