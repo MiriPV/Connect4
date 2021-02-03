39d9f8c0370abb1d021b03fb518932dc1f7701f9
MiriPV <mirandaverheij93@gmail.com>
2021-02-03 15:08:01 +0100

    Winning posibilities are added. Command line version is done.

--- connect4.py
+++ connect4.py
@@ -4,15 +4,15 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, colom, piece):
     board[row][colom] = piece 
 
 def is_valid_location(board, colom):
-    #returns false, if top row (5) is full
-    return board[5][colom] == 0
+    #returns false, if top row is full
+    return board[ROW_COUNT - 1][colom] == 0
 
 def get_next_open_row(board, colom):
     for r in range(ROW_COUNT):
@@ -21,6 +21,33 @@ def get_next_open_row(board, colom):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    #Check horizontal locations for win. The last three colums don't have to be checked,
+    #because there can no longer be 4 in a row if the columns before that were empty.
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    #Check vertical locations for win.
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    
+    #Check diagonals positively sloped for win.
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    #Check diagonals negatively sloped for win.
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
 
 board = create_board()
 print_board(board)
@@ -36,6 +63,9 @@ while not game_over:
             row = get_next_open_row(board, colom)
             drop_piece(board, row, colom, 1)
 
+            if winning_move(board, 1):
+                print("PLAYER 1 WINS!")
+
 
 
     #Ask for input player 2
@@ -45,6 +75,9 @@ while not game_over:
         if is_valid_location(board, colom):
             row = get_next_open_row(board, colom)
             drop_piece(board, row, colom, 2)
+
+            if winning_move(board, 2):
+                print("PLAYER 2 WINS!")
 
     print_board(board)
 
